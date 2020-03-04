798f56a2a8d2e3bf90c646cb4c0ce198602fa5ad
Alejandro <alexbernal32396@gmail.com>
2020-03-04 10:42:46 -0800

    Added week 5 folder and Homework3.

---
+++ Week5/Homework3.js
@@ -0,0 +1,30 @@
+// WEEK 5: Homework 3
+// By: Alejandro Bernal
+
+// Problem 1: Using callbacks syntax write a set of functions that checks and makes a list of websites running(20pt)
+const websites = [{
+        site: 'yahoo.com',
+        responseTime: 50,
+        unit: 'ms'
+    },
+    {
+        site: 'google.com',
+        responseTime: 10,
+        unit: 'ms'
+    },
+    {
+        site: 'reddit.com',
+        responseTime: null,
+        unit: 'ms'
+    },
+    {
+        site: 'slack.com',
+        responseTime: 80,
+        unit: 'ms'
+    },
+    {
+        site: 'github.com',
+        responseTime: 30,
+        unit: 'ms'
+    }
+];
