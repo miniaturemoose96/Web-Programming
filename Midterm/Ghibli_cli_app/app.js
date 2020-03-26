a682e5209e30da6d1d24624eabb9b61f7416edd0
Alejandro <alexbernal32396@gmail.com>
2020-03-25 22:51:25 -0700

    Finished Midterm Requirements, now we need to add the index.js file.

--- Midterm/Ghibli_cli_app/app.js
+++ Midterm/Ghibli_cli_app/app.js
@@ -2,175 +2,41 @@ const figlet = require('figlet'); // adds ascii art headers
 const superagent = require('superagent');
 const inquirer = require('inquirer');
 
-// Helper to display data
-const _displayInformation = (response, category) => {
+// Helper function to have a nice display of film objects
+const _displayFilmInformation = (response) => {
+    figlet(`${response.title}`, {
+        font: 'Small',
+        horizontalLayout: 'default',
+        verticalLayout: 'default'
+    }, function(err, data) {
+        if (err) {
+            console.log('Something went wrong...');
+            console.dir(err);
+            return;
+        }
+        console.log(data)
+    });
 
-    switch (category) {
-        case 'films':
-            figlet(`${response.title}`, {
-                font: 'Small',
-                horizontalLayout: 'default',
-                verticalLayout: 'default'
-            }, function(err, data) {
-                if (err) {
-                    console.log('Something went wrong...');
-                    console.dir(err);
-                    return;
-                }
-                console.log(data)
-            });
+    console.log(`Description: ${response.description}\n`);
+    console.log(`Release Year: ${response.release_date}\n`);
+    console.log(`Rating: ${response.rt_score}`);
 
-            console.log(`Description: ${response.description}\n`);
-            console.log(`Release Year: ${response.release_date}\n`);
-            console.log(`Rating: ${response.rt_score}`);
-
-            break;
-        case 'people':
-            figlet(`${response.name}`, {
-                font: 'Small',
-                horizontalLayout: 'default',
-                verticalLayout: 'default'
-            }, function(err, data) {
-                if (err) {
-                    console.log('Something went wrong...');
-                    console.dir(err);
-                    return;
-                }
-                console.log(data)
-            });
-
-            console.log(`Gender: ${response.gender}\n`);
-            console.log(`Age: ${response.age}\n`);
-            console.log(`Eye Color: ${response.eye_color}, Hair Color: ${response.hair_color}`);
-
-            break;
-        case 'locations':
-            figlet(`${response.name}`, {
-                font: 'Small',
-                horizontalLayout: 'default',
-                verticalLayout: 'default'
-            }, function(err, data) {
-                if (err) {
-                    console.log('Something went wrong...');
-                    console.dir(err);
-                    return;
-                }
-                console.log(data)
-            });
-
-            console.log(`Climate: ${response.climate}\n`);
-            console.log(`Terrain: ${response.terrain}\n`);
-            console.log(`Surface Water: ${response.surface_water}`);
-            break;
-        case 'species':
-            figlet(`${response.name}`, {
-                font: 'Small',
-                horizontalLayout: 'default',
-                verticalLayout: 'default'
-            }, function(err, data) {
-                if (err) {
-                    console.log('Something went wrong...');
-                    console.dir(err);
-                    return;
-                }
-                console.log(data)
-            });
-
-            console.log(`Classification: ${response.classification}\n`);
-            console.log(`Eye Colors: ${response.eye_colors}\n`);
-            console.log(`Hair Colors: ${response.hair_colors}`);
-
-            break;
-        case 'vehicles':
-            figlet(`${response.name}`, {
-                font: 'Small',
-                horizontalLayout: 'default',
-                verticalLayout: 'default'
-            }, function(err, data) {
-                if (err) {
-                    console.log('Something went wrong...');
-                    console.dir(err);
-                    return;
-                }
-                console.log(data)
-            });
-
-            console.log(`Description: ${response.description}\n`);
-            console.log(`Type: ${response.vehicle_class}\n`);
-            console.log(`Size: ${response.length}`);
-
-            break;
-        default:
-            break;
-    }
 };
 
-// Helper to display Id of items
-// const _displayCategoryItems = (response, category) => {
-//     if (category === 'films') {
-
-//         figlet(`${category}`, {
-//             font: 'Small',
-//             horizontalLayout: 'full',
-//             verticalLayout: 'full'
-//         }, function(err, data) {
-//             if (err) {
-//                 console.log('Something went wrong...');
-//                 console.dir(err);
-//                 return;
-//             }
-//             console.log(data)
-//         });
-
-//         let count_films = 0;
-//         response.forEach(film => {
-//             count_films += 1;
-//             console.log(`${count_films}) ${film.title}`);
-//         });
-
-//     } else {
-//         figlet(`${category}`, {
-//             font: 'Small',
-//             horizontalLayout: 'full',
-//             verticalLayout: 'full'
-//         }, function(err, data) {
-//             if (err) {
-//                 console.log('Something went wrong...');
-//                 console.dir(err);
-//                 return;
-//             }
-//             console.log(data)
-//         });
-
-//         let count_elem = 0;
-//         response.forEach(elem => {
-//             count_elem += 1;
-//             console.log(`${count_elem}) ${elem.name}`);
-//         });
-
-//     }
-// };
-
-// Function will create a list of films, people, locations, species or vehicles
-// call thsi function if the user does not pass a keyword to search category
+// Function will create a list of films
 // returns list to use with inquirer prompt
-const _createList = (response, category) => {
+const _createFilmList = (response) => {
     let listOfItems = [];
-    if (category === 'films') {
-        response.forEach(film => {
-            listOfItems.push(film.title);
-        });
-    } else {
-        response.forEach(elem => {
-            listOfItems.push(elem.name);
-        });
-    }
+
+    response.forEach(film => {
+        listOfItems.push(film.title);
+    });
 
     return listOfItems;
 };
 
 // Allow user to search selected category item by id list and display info
-const _selectItemInCategory = (listOfId) => {
+const _selectFilmInCategory = (listOfId) => {
     return inquirer.prompt([{
         type: 'rawlist',
         name: 'item',
@@ -179,35 +45,27 @@ const _selectItemInCategory = (listOfId) => {
     }]);
 };
 
-// gets studio ghibli films
-// Uses lowercase to allow user to search by keyword, gets one film based on id
-const _getItemId = (response, keyword, category) => {
-    let itemId = [];
-    if (category === 'films') {
-        response.forEach(film => {
-            const { id, title } = film;
-            const filmTitle = title.toLowerCase();
-            if (filmTitle.includes(keyword)) {
-                itemId.push(id);
-            }
-        });
 
-    } else {
-        response.forEach(item => {
-            const { id, name } = item;
-            const itemName = name.toLowerCase();
-            if (itemName.includes(keyword)) {
-                itemId.push(id);
-            }
-        });
-    }
+// gets studio ghibli films
+// Uses lowercase to allow user to search by keyword
+// returns list of id of those that include keyword
+const _getFilmByKeyword = (response, keyword) => {
+    let itemId = [];
+
+    response.forEach(film => {
+        const { id, title } = film;
+        const filmTitle = title.toLowerCase();
+        if (filmTitle.includes(keyword)) {
+            itemId.push(id);
+        }
+    });
 
     return itemId;
 };
 
 // This function shall allow us to create a connection to Ghibli API
 // takes one parameter by default set to films, displays all films by Studio Ghibli
-async function search(category = 'films', keyword = '') {
+async function search(keyword = '') {
     // Banner 
     figlet('Studio Ghibli API', {
         font: 'Small',
@@ -222,38 +80,50 @@ async function search(category = 'films', keyword = '') {
         console.log(data)
     });
 
-    const ghibli_base = 'https://ghibliapi.herokuapp.com';
-    const choiceUrl = `${ghibli_base}/${category}`;
-    const baseUrlResponse = await superagent.get(choiceUrl);
+    // add to index js
+    const ghibli_base = 'https://ghibliapi.herokuapp.com/films';
+    const baseUrlResponse = await superagent.get(ghibli_base);
     // loop through the response and display
-    const itemsResponse = baseUrlResponse.body;
+    const filmsResponse = baseUrlResponse.body;
+    const filmsList = _createFilmList(filmsResponse);
 
+    // To check by keyword option
     if (keyword === '') {
-        const listOfItems = _createList(itemsResponse, category);
-        const selectItem = await _selectItemInCategory(listOfItems);
-        if (category === 'films' && selectItem.item) {
-            itemsResponse.forEach(film => {
-                if (film.title.includes(selectItem.item)) {
-                    console.log(film.id);
+        // Prompt
+        const selectFilm = await _selectFilmInCategory(filmsList);
+
+        let id = '';
+        // Get the ID of the film
+        if (selectFilm.item) {
+            for (let i = 0; i < filmsResponse.length; i++) {
+                if (selectFilm.item === filmsResponse[i].title) {
+                    id = filmsResponse[i].id;
                 }
-            });
-        } else {
-            itemsResponse.forEach(item => {
-                if (item.name.includes(selectItem.item)) {
-                    console.log(item.id);
-                }
-            });
+            }
         }
+        // Create new connection 
+        // Display Contents
+        const selectedfilmUrl = `${ghibli_base}/${id}`;
+        const selectedfilmResponse = await superagent.get(selectedfilmUrl);
+        const selectedFilmBody = selectedfilmResponse.body;
+        _displayFilmInformation(selectedFilmBody);
     } else {
-        const itemID = _getItemId(itemsResponse, keyword, category);
-        for (let i = 0; i < itemID.length; i++) {
-            const singleItemUrl = `${choiceUrl}/${itemID[i]}`;
-            const singleItemResponse = await superagent.get(singleItemUrl);
-            const singleItemBody = singleItemResponse.body;
-            _displayInformation(singleItemBody, category);
+        // Get list of those movie that contain the given keyword
+        const getIDlist = _getFilmByKeyword(filmsResponse, keyword);
+        if (getIDlist.length == 0) {
+            console.log(`No results for keyword --> ${keyword}`);
+        } else {
+            console.log(`Found ${getIDlist.length} result(s) for Keyword: ${keyword}`);
+            for (let i = 0; i < getIDlist.length; i++) {
+                // Construct url
+                const keyfoundUrl = `${ghibli_base}/${getIDlist[i]}`;
+                const keyfoundResponse = await superagent.get(keyfoundUrl);
+                const keyfoundBody = keyfoundResponse.body;
+                _displayFilmInformation(keyfoundBody);
+            }
+
         }
     }
-
 };
 
 module.exports = {
--- Midterm/Ghibli_cli_app/cli.js
+++ Midterm/Ghibli_cli_app/cli.js
@@ -8,17 +8,13 @@ yargs
         desc: 'Search Studio Ghibli API categories.',
         builder: yargs => {
             return yargs
-                .options('c', {
-                    alias: 'category',
-                    describe: 'use these categories: films, people, locations, species and vehicles.'
-                })
                 .options('k', {
                     alias: 'keyword',
                     describe: 'search categories using a keyword, try castle or totoro'
                 });
         },
         handler: argv => {
-            app.search(argv.category, argv.keyword);
+            app.search(argv.keyword);
         }
     })
     // add a help menu to assist the user in understanding our CLI
