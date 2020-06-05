a3d373cd8b5280f212263f8dc7cbb5888f46367b
Alejandro <alexbernal32396@gmail.com>
2020-06-05 02:44:27 -0700

    HW1 corrections completed. Ready for MERGE to master.

--- Week2/Homework1.js
+++ Week2/Homework1.js
@@ -1,26 +1,24 @@
 // Problem 4: Counting Letters
 
-function countingLetters(quote, punctuations) {
-    let obj = {};
-    let rem_punct = '';
+const countingLetters = (quote, punctuations) => {
+    const letterCounts = {};
 
-    let low_quote = quote.toLowerCase();
-    for (let i = 0; i < low_quote.length; i++) {
-        let char = low_quote[i];
-        // Check for punctuations remove and add to a new string
-        if (punctuations.includes(char)) {
-            // Skip This part
-        } else {
-            if (Object.keys(obj).includes(char)) {
-                obj[char] += 1;
+    for (let i = 0; i < quote.length; i++) {
+        const character = quote[i];
+
+        if (!punctuations.includes(character)) {
+            const lowerQuote = character.toLowerCase();
+
+            if (letterCounts[lowerQuote]) {
+                letterCounts[lowerQuote] = letterCounts[lowerQuote] + 1;
             } else {
-                obj[char] = 1;
+                letterCounts[lowerQuote] = 1;
             }
         }
     }
 
-    return obj;
-}
+    return letterCounts;
+};
 
 const quote = 'The greatest glory in living lies not in never falling, but in rising every time we fall.';
 const punctuations = [' ', ',', '.', '?', '!', ';', ':', '"', "'"];
@@ -29,22 +27,21 @@ console.log(countingLetters(quote, punctuations));
 
 // Problem 5: Compare Arrays
 
-function compareArrays(arrayOne, arrayTwo) {
-    const arr1 = arrayOne;
-    const arr2 = arrayTwo;
-    let sameArray = false;
+const compareArrays = (arrayOne, arrayTwo) => {
+    if (arrayOne.length !== arrayTwo.length) {
+        return false;
+    }
 
     for (let i = 0; i < arrayOne.length; i++) {
-        for (let j = 0; j < arrayTwo.length; j++) {
-            if (arr1[i] === arr2[j]) {
-                sameArray = true;
-            } else {
-                sameArray = false;
-            }
+        const elemOne = arrayOne[i];
+        const elemTwo = arrayTwo[i];
+
+        if (elemOne !== elemTwo) {
+            return false;
         }
     }
-    return sameArray;
-}
+    return true;
+};
 
 const array_a1 = [1, 2, 3];
 const array_a2 = [1, 2, 3];
@@ -57,20 +54,19 @@ console.log(compareArrays(array_a3, array_a4)); // OUTPUT: False
 
 // Problem 6: Convert to an Object
 
-function convertToObject(gameArray) {
-    let key = '';
-    let value = '';
-    let gameObject = {};
+const convertToObject = (gameArray) => {
+    const gameObj = {};
+
     for (let i = 0; i < gameArray.length; i++) {
-        const inner = gameArray[i].length;
-        for (let j = 0; j < inner; j++) {
-            key = gameArray[i][0];
-            value = gameArray[i][1];
-        }
-        gameObject[key] = value;
+        const set = gameArray[i]
+
+        const key = set[0];
+        const value = set[1];
+
+        gameObj[key] = value;
     }
-    return gameObject;
-}
+    return gameObj;
+};
 
 const game_array = [
     ['developer', 'Respawn'],
@@ -84,7 +80,7 @@ console.log(convertToObject(game_array));
 
 // Problem 7: Build an Object
 
-function buildObject(array) {
+const buildObject = (array) => {
     let productObject = {};
     let prop = '';
     let asgn = '';
@@ -101,7 +97,7 @@ function buildObject(array) {
     }
 
     return productObject;
-}
+};
 
 const products = [
     [{
